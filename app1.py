2a8bf8316ad4840b750d09b4a0c48abcc06a9c8c
JanBudna <jan.budna@gmail.com>
2026-03-26 12:01:15 +0000

    com 1

---
+++ app1.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, session
+from tinydb import TinyDB, Query
+
+app = Flask(
+    __name__,
+    template_folder="templates1",
+    static_folder="static1"
+)
+app.secret_key = "adminCode08"
+
+db = TinyDB("database.json")
+users = db.table("users")
+User = Query()
+
+@app.route("/")
+def homePage():
+    if "user" in session:
+        return redirect("/dashboard")
+    return redirect("/login")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.search(User.username == username):
+            return "Uporabnik že obstaja"
+
+        users.insert({"username" : username, "password" : password, "note" : ""})
+        return redirect("/login")
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = users.get(User.username == username)
+        
+        if user and user["password"] == password:
+            session["user"] = username
+            return redirect("/dashboard")
+        else:
+            return "Napačen login"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect("/login")
+    
+    return render_template("dashboard.html", user=session["user"])
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
